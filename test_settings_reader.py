1158580ca764c4db7aacd8fb7d93f5813039f5c8
Tymoteusz Podlasz <01206497@pw.edu.pl>
2026-03-31 11:12:25 +0200

    tested create_components() function and fixed error

--- test_settings_reader.py
+++ test_settings_reader.py
@@ -1,4 +1,5 @@
 from settings_reader import open_file, create_components
+import pytest
 
 starting_parameters = [
     {
@@ -48,30 +49,27 @@ parameter_settings = [
     }
 ]
 
-component_settings = [
-    {
-        "meters": [
-            "ph",
-            "temperature",
-            "conductivity"
-        ],
-        "regulators": [
-            "ph",
-            "temperature",
-            "conductivity"
-        ],
-        "other": [
-            "lights"
-        ]
-    }
-]
-
 
 def test_open_file():
-    file = open_file("starting_parameters.json")
+    file = open_file('starting_parameters.json')
     assert file == starting_parameters
 
 
 def test_open_file2():
-    file = open_file("parameter_settings.json")
+    file = open_file('parameter_settings.json')
     assert file == parameter_settings
+
+
+def test_open_file_not_json():
+    with pytest.raises(ValueError):
+        open_file('parameter_settings.txt')
+
+
+def test_create_components():
+    settings = open_file('starting_parameters.json')
+    path = 'component_settings.json'
+    tanks, meters, regulators, other = create_components(path, settings)
+    assert len(tanks) == 1
+    assert len(meters) == 3
+    assert len(regulators) == 3
+    assert len(other) == 0
