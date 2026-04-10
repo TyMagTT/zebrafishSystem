089bfeaff1ac61c545a0a2a55b99feae9908fb37
Tymoteusz Podlasz <01206497@pw.edu.pl>
2026-04-10 10:49:11 +0200

    implemented running simulation using the execute_state() function, added exiting program

--- simulation.py
+++ simulation.py
@@ -121,13 +121,15 @@ def select_option(select_msg, again_msg, options):
             print(again_msg)
 
 
-def set_state(state, command):
+def next_state(state, command):
     match state:
         case 0:
             if command == 'run':
                 return 1
             if command == 'edit':
                 return 5
+            if command == 'exit':
+                return -1
         case 1:
             return 2
         case 2:
@@ -138,7 +140,10 @@ def set_state(state, command):
         case 3:
             return 4
         case 4:
-            return 0
+            if command == 'no':
+                return 40
+            if command == 'yes':
+                return 41
         case 5:
             pass  # multiple tanks ????
         case 6:
@@ -168,6 +173,10 @@ def set_state(state, command):
                 return 82
             if command == 'back':
                 return 5
+        case 40:
+            return 0
+        case 41:
+            return 0
     if state >= 50 and state < 60:
         if command == 'simulation':
             return 6
@@ -178,8 +187,45 @@ def set_state(state, command):
 
 def execute_state(state):
     match state:
+        case -1:
+            quit('\nProgram terminated by user')
         case 0:
-            pass
+            command = select_option(msg['select_mode'], msg['again'], ['run', 'edit', 'exit'])
+            data = None
+            return command, data
+        case 1:
+            command = None
+            number = False
+            while not number:
+                message = f'{msg['select_duration']}\n'
+                data = input(message)
+                try:
+                    data = int(data)
+                    number = True
+                except ValueError:
+                    print(msg['again'])
+            return command, data
+        case 2:
+            command = select_option(msg['begin'], msg['again'], ['begin', 'back'])
+            data = None
+            return command, data
+        case 3:
+            command = None
+            data = simulate(tanks, meters, controller, frame_number, wait_time)
+            return command, data
+        case 4:
+            create_graph(frame_number, saved_values, msg)
+            command = select_option(msg['save_graph'], msg['again'], ['no', 'yes'])
+            data = None
+            return command, data
+        case 40:
+            command = None
+            data = None
+            return command, data
+        case 41:
+            command = None
+            data = None
+            return command, data
     raise ValueError
 
 
@@ -189,10 +235,20 @@ msg = select_language(language_file)
 print(msg['test'])
 
 state = 0
-command = select_option(msg['select_mode'], msg['again'], ['run', 'settings'])
-minute_duration = 0.03
+frame_number = 0
 wait_time = 0.001
-frame_number = floor(minute_duration * 60 / wait_time)
-saved_values = simulate(tanks, meters, controller, frame_number, wait_time)
+saved_values = {}
 fig, ax = plt.subplots(3, 1)
-create_graph(frame_number, saved_values, msg)
+on = True
+
+while on:
+    command, data = execute_state(state)
+    if state == 1:
+        second_duration = data
+        frame_number = floor(second_duration / wait_time)
+    elif state == 3:
+        saved_values = data
+    state = next_state(state, command)
+
+# saved_values = simulate(tanks, meters, controller, frame_number, wait_time)
+# create_graph(frame_number, saved_values, msg)
